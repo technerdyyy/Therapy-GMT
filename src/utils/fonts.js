77e4765cbb713ca1beefae44ada25ff023e60010
technerdyyy <akansha7803@gmail.com>
2026-02-01 17:32:40 +0530

    fonts added

---
+++ src/utils/fonts.js
@@ -0,0 +1,77 @@
+import localFont from "next/font/local";
+
+export const gopher = localFont({
+  src: [
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-thin.otf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-thinitalic.otf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-lightitalic.otf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-italic.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-mediumitalic.otf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-bolditalic.otf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-heavy.otf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-heavyitalic.otf",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-black.otf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/fonnts.com-gopher-blackitalic.otf",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-gopher",
+});
